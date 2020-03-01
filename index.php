700c71c9d6ff499dbbae1218060d00ac5b9830ba
Arpit sharma <60608608+Arpitsh018@users.noreply.github.com>
2020-03-01 15:46:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,121 +5,164 @@
 	*@Author - Arpit sharma
 	*/
 
-	function FetchDataWithAes($SearchData,$RequiredData,$DatabaseConnection,$DbTableName,$EncodeAndEncryptPass,$CheckFor = 'any' ,$CheckUserStatus = NULL,$FetchCount = NULL){
-		if((strlen($SearchData) == 0 && $SearchData != 'none') || (strlen($RequiredData) == 0 && $RequiredData != NULL)){
-			return ["status"=>"Error","msg"=>"Search Data or Required Data not found","code"=>400];
+	function FetchDataWithAes($Condtion,$FetchColumn,$DatabaseConnection,$DbTableName,$EncodeAndEncryptPass,$CheckFor = 'any' ,$CheckUserStatus = NULL,$FetchCount = NULL,$AcceptNullCondtion=false){
+		if((strlen($Condtion) == 0 && $Condtion != 'none') || (strlen($FetchColumn) == 0 && $FetchColumn != NULL)){
+			return ["status"=>"Error","msg"=>"Invalid Given Data formet detect!","code"=>400];
 		}
-		
-		if($SearchData != 'none'){
-			// Get search data in array
-			$SearchDataArray = explode("::,::",$SearchData);
-			$StmtSearchKey = array();
-			$StmtSearchValue = array();
+
+		if($Condtion != 'none'){
+			// Get Condtion data in array
+			$CondtionArray = explode("::,::",$Condtion);
+			$StmtCondtionKey = array();
+			$StmtCondtionValue = array();
 			$i = 0;
-			foreach ($SearchDataArray as $value) {
+			foreach ($CondtionArray as $value) {
 				$i++;
 				
-				if(strpos($value, "::::") !== false || $CheckFor === 'NotEqualAny' || $CheckFor === 'NotEqualAll'){
-				   $TmpSearchDataArray = explode("::::",$value);
+				if(strpos($value, "::::") !== false || $CheckFor === 'NotEqualAny' || $CheckFor === 'NotEqualAll' || $AcceptNullCondtion === true){
+				   $TmpCondtionArray = explode("::::",$value);
 				} else{
-				   return ["status"=>"Error","msg"=>"Search Data key ($value) in invalid formate","code"=>400]; exit();
+				   return ["status"=>"Error","msg"=>"Invalid given data format detect!","code"=>400];
 				}
 					
-				if(preg_replace("/[^A-Za-z0-9_]/","",$TmpSearchDataArray[0]) !== "" && preg_replace("/[^A-Za-z0-9_]/","",$TmpSearchDataArray[0]) === $TmpSearchDataArray[0] && (preg_replace("/^[ ]/","",$TmpSearchDataArray[1]) !== "" || $CheckFor === 'NotEqualAny' || $CheckFor === 'NotEqualAll')){
+				if(preg_replace("/[^A-Za-z0-9_]/","",$TmpCondtionArray[0]) !== "" && preg_replace("/[^A-Za-z0-9_]/","",$TmpCondtionArray[0]) === $TmpCondtionArray[0] && (preg_replace("/^[ ]/","",$TmpCondtionArray[1]) !== "" || $CheckFor === 'NotEqualAny' || $CheckFor === 'NotEqualAll' || $AcceptNullCondtion === true)){
+					if($TmpCondtionArray[1] == '' && ($CheckFor == 'StartLike' || $CheckFor == 'LikeLast' || $CheckFor == 'StartLikeLast')){
+						return ["status"=>"Error","msg"=>"Null condtion value  not support with StartLike, LikeLast or StartLikeLast","code"=>400];
+					}
 					if($i === 1){
 						if($CheckFor === 'any' || $CheckFor === 'all'){
-							$StmtSearchKeyAndPreparedKey = $TmpSearchDataArray[0]." = AES_ENCRYPT(:".$TmpSearchDataArray[0]."Srh, :EncodeAndEncryptPass)";
-						}else if($CheckFor === 'StartLikeAny' || $CheckFor === 'LikeLastAny' || $CheckFor === 'StartLikeLastAny'){
-							$StmtSearchKeyAndPreparedKey = "AES_DECRYPT(".$TmpSearchDataArray[0].", :EncodeAndEncryptPass) LIKE :".$TmpSearchDataArray[0].'Srh';
+							if($TmpCondtionArray[1] == ''){
+								$StmtCondtionKeyAndPreparedKey = $TmpCondtionArray[0]." is null";
+							}else if($TmpCondtionArray[1] === 'NullAndNothing'){
+								$StmtCondtionKeyAndPreparedKey = $TmpCondtionArray[0]." = ''";
+							}else{
+								$StmtCondtionKeyAndPreparedKey = $TmpCondtionArray[0]." = AES_ENCRYPT(:".$TmpCondtionArray[0].$i."Srh, :EncodeAndEncryptPass)";
+							}
+						}else if($CheckFor === 'StartLike' || $CheckFor === 'LikeLast' || $CheckFor === 'StartLikeLast'){
+							$StmtCondtionKeyAndPreparedKey = "AES_DECRYPT(".$TmpCondtionArray[0].", :EncodeAndEncryptPass) LIKE :".$TmpCondtionArray[0].$i.'Srh';
 						}else if($CheckFor === 'NotEqualAny' || $CheckFor === 'NotEqualAll'){
-							$StmtSearchKeyAndPreparedKey = $TmpSearchDataArray[0]." != AES_ENCRYPT(:".$TmpSearchDataArray[0]."Srh, :EncodeAndEncryptPass)";
+							if($TmpCondtionArray[1] == ''){
+								$StmtCondtionKeyAndPreparedKey = $TmpCondtionArray[0]." is not null";
+							}else if($TmpCondtionArray[1] === 'NullAndNothing'){
+								$StmtCondtionKeyAndPreparedKey = $TmpCondtionArray[0]." = ''";
+							}else{
+								$StmtCondtionKeyAndPreparedKey = $TmpCondtionArray[0]." != AES_ENCRYPT(:".$TmpCondtionArray[0].$i."Srh, :EncodeAndEncryptPass)";
+							}
 						}else{
-							return ["status"=>"Error","msg"=>"Invalid CheckFo detect","code"=>400];
+							return ["status"=>"Error","msg"=>"Invalid CheckFor details detect!","code"=>400];
 						}
 					}else{
 						if($CheckFor === 'any'){
-							$StmtSearchKeyAndPreparedKey = $StmtSearchKeyAndPreparedKey." || ".$TmpSearchDataArray[0]." = AES_ENCRYPT(:".$TmpSearchDataArray[0]."Srh, :EncodeAndEncryptPass)";
+							if($TmpCondtionArray[1] == ''){
+								$StmtCondtionKeyAndPreparedKey = $StmtCondtionKeyAndPreparedKey." || ".$TmpCondtionArray[0]." is null";
+							}else if($TmpCondtionArray[1] === 'NullAndNothing'){
+								$StmtCondtionKeyAndPreparedKey = $TmpCondtionArray[0]." = ''";
+							}else{
+								$StmtCondtionKeyAndPreparedKey = $StmtCondtionKeyAndPreparedKey." || ".$TmpCondtionArray[0]." = AES_ENCRYPT(:".$TmpCondtionArray[0].$i."Srh, :EncodeAndEncryptPass)";
+							}
 						}else if($CheckFor === 'all'){
-							$StmtSearchKeyAndPreparedKey = $StmtSearchKeyAndPreparedKey." && ".$TmpSearchDataArray[0]." = AES_ENCRYPT(:".$TmpSearchDataArray[0]."Srh, :EncodeAndEncryptPass)";
-						}else if($CheckFor === 'StartLikeAny' || $CheckFor === 'LikeLastAny' || $CheckFor === 'StartLikeLastAny'){
-							$StmtSearchKeyAndPreparedKey = $StmtSearchKeyAndPreparedKey." || AES_DECRYPT(".$TmpSearchDataArray[0].", :EncodeAndEncryptPass) LIKE :".$TmpSearchDataArray[0].'Srh';
+							if($TmpCondtionArray[1] == ''){
+								$StmtCondtionKeyAndPreparedKey = $StmtCondtionKeyAndPreparedKey." && ".$TmpCondtionArray[0]." is null";
+							}else if($TmpCondtionArray[1] === 'NullAndNothing'){
+								$StmtCondtionKeyAndPreparedKey = $TmpCondtionArray[0]." = ''";
+							}else{
+								$StmtCondtionKeyAndPreparedKey = $StmtCondtionKeyAndPreparedKey." && ".$TmpCondtionArray[0]." = AES_ENCRYPT(:".$TmpCondtionArray[0].$i."Srh, :EncodeAndEncryptPass)";
+							}
+						}else if($CheckFor === 'StartLike' || $CheckFor === 'LikeLast' || $CheckFor === 'StartLikeLast'){
+							$StmtCondtionKeyAndPreparedKey = $StmtCondtionKeyAndPreparedKey." || AES_DECRYPT(".$TmpCondtionArray[0].", :EncodeAndEncryptPass) LIKE :".$TmpCondtionArray[0].$i.'Srh';
 						}else if($CheckFor === 'NotEqualAny'){
-							$StmtSearchKeyAndPreparedKey = $StmtSearchKeyAndPreparedKey." || ".$TmpSearchDataArray[0]." != AES_ENCRYPT(:".$TmpSearchDataArray[0]."Srh, :EncodeAndEncryptPass)";
+							if($TmpCondtionArray[1] == ''){
+								$StmtCondtionKeyAndPreparedKey = $StmtCondtionKeyAndPreparedKey." || ".$TmpCondtionArray[0]." is not null";
+							}else if($TmpCondtionArray[1] === 'NullAndNothing'){
+								$StmtCondtionKeyAndPreparedKey = $TmpCondtionArray[0]." = ''";
+							}else{
+								$StmtCondtionKeyAndPreparedKey = $StmtCondtionKeyAndPreparedKey." || ".$TmpCondtionArray[0]." != AES_ENCRYPT(:".$TmpCondtionArray[0].$i."Srh, :EncodeAndEncryptPass)";
+							}
 						}else if($CheckFor === 'NotEqualAll'){
-							$StmtSearchKeyAndPreparedKey = $StmtSearchKeyAndPreparedKey." && ".$TmpSearchDataArray[0]." != AES_ENCRYPT(:".$TmpSearchDataArray[0]."Srh, :EncodeAndEncryptPass)";
+							if($TmpCondtionArray[1] == ''){
+								$StmtCondtionKeyAndPreparedKey = $StmtCondtionKeyAndPreparedKey." && ".$TmpCondtionArray[0]." is not null";
+							}else if($TmpCondtionArray[1] === 'NullAndNothing'){
+								$StmtCondtionKeyAndPreparedKey = $TmpCondtionArray[0]." = ''";
+							}else{
+								$StmtCondtionKeyAndPreparedKey = $StmtCondtionKeyAndPreparedKey." && ".$TmpCondtionArray[0]." != AES_ENCRYPT(:".$TmpCondtionArray[0].$i."Srh, :EncodeAndEncryptPass)";
+							}
 						}else{
-							return ["status"=>"Error","msg"=>"Invalid CheckFo detect","code"=>400];
+							return ["status"=>"Error","msg"=>"Invalid CheckFor details detect!","code"=>400];
 						}
 					}
-					array_push($StmtSearchKey, $TmpSearchDataArray[0].'Srh');
-					array_push($StmtSearchValue, $TmpSearchDataArray[1]);
+					
+					if($TmpCondtionArray[1] != '' && $TmpCondtionArray[1] != 'NullAndNothing'){
+						array_push($StmtCondtionKey, $TmpCondtionArray[0].$i.'Srh');
+						array_push($StmtCondtionValue, $TmpCondtionArray[1]);
+					}
 				}else{
-					return ["status"=>"Error","msg"=>"Search Data key (".$TmpSearchDataArray[0].") or value (".$TmpSearchDataArray[1].") contaion invalid character","code"=>400];
+					return ["status"=>"Error","msg"=>"NULL key and value not support without AcceptNullCondtion","code"=>400];
 				}
 				
 			}
 		}else{
-			$StmtSearchKeyAndPreparedKey = '0=0';
-			$StmtSearchKey = array();
-			$StmtSearchValue = array();
+			$StmtCondtionKeyAndPreparedKey = '0=0';
+			$StmtCondtionKey = array();
+			$StmtCondtionValue = array();
 		}
-
-		if($RequiredData != NULL){
+		
+		if($FetchColumn != NULL){
 			// Get required data in array
-			$RequiredDataArray = explode("::::",$RequiredData);
+			$FetchColumnArray = explode("::::",$FetchColumn);
 			$i = 0;
-			foreach ($RequiredDataArray as $value){
+			foreach ($FetchColumnArray as $value) {
 				$i++;
 					
 				if(preg_replace("/[^A-Za-z0-9_]/","",$value) !== "" && preg_replace("/[^A-Za-z0-9_]/","",$value) === $value){
 					if($i === 1){
-						$StmtRequiredDataKey ="AES_DECRYPT(".$value.", :EncodeAndEncryptPass) AS ".$value;
+						$StmtFetchColumnKey ="AES_DECRYPT(".$value.", :EncodeAndEncryptPass) AS ".$value;
 					}else{
-						$StmtRequiredDataKey = $StmtRequiredDataKey.", AES_DECRYPT(".$value.", :EncodeAndEncryptPass) AS ".$value;
+						$StmtFetchColumnKey = $StmtFetchColumnKey.", AES_DECRYPT(".$value.", :EncodeAndEncryptPass) AS ".$value;
 					}
 				}else{
-					return ["status"=>"Error","msg"=>"Required Data ($value) contaion invalid character","code"=>400];
+					return ["status"=>"Error","msg"=>"Invalid Fatch column name detect","code"=>400];
 				}
 			}
 		}else{
-			$StmtRequiredDataKey = NULL;
+			$StmtFetchColumnKey = NULL;
 		}
 
-		if(isset($DatabaseConnection) && isset($DbTableName) && (isset($StmtRequiredDataKey) || $RequiredData == NULL) && isset($StmtSearchValue) && isset($StmtSearchKey) && isset($StmtSearchKeyAndPreparedKey) && isset($EncodeAndEncryptPass)){
+		if(isset($DatabaseConnection) && isset($DbTableName) && (isset($StmtFetchColumnKey) || $FetchColumn == NULL) && isset($StmtCondtionValue) && isset($StmtCondtionKey) && isset($StmtCondtionKeyAndPreparedKey) && isset($EncodeAndEncryptPass)){
 			if($CheckUserStatus !== NULL){
-				$StmtSearchKeyAndPreparedKey = '('.$StmtSearchKeyAndPreparedKey.')  && Status = AES_ENCRYPT(:Status, :EncodeAndEncryptPass)';
+				$StmtCondtionKeyAndPreparedKey = '('.$StmtCondtionKeyAndPreparedKey.')  && Status = AES_ENCRYPT(:Status, :EncodeAndEncryptPass)';
 			}
-			if($RequiredData == NULL){
-				$stmt = $DatabaseConnection->prepare("SELECT NULL FROM $DbTableName WHERE $StmtSearchKeyAndPreparedKey");
+			//return_response("SELECT $StmtFetchColumnKey FROM $DbTableName WHERE $StmtCondtionKeyAndPreparedKey");
+			if($FetchColumn == NULL){
+				$stmt = $DatabaseConnection->prepare("SELECT NULL FROM $DbTableName WHERE $StmtCondtionKeyAndPreparedKey");
 			}else{
-				$stmt = $DatabaseConnection->prepare("SELECT $StmtRequiredDataKey FROM $DbTableName WHERE $StmtSearchKeyAndPreparedKey");
+				$stmt = $DatabaseConnection->prepare("SELECT $StmtFetchColumnKey FROM $DbTableName WHERE $StmtCondtionKeyAndPreparedKey");
 			}
 
 			$stmt->bindValue(':EncodeAndEncryptPass', $EncodeAndEncryptPass, PDO::PARAM_STR);
 			$i = 0;
-			if($StmtSearchKeyAndPreparedKey != '0=0'){
+			if($StmtCondtionKeyAndPreparedKey != '0=0'){
 				if($CheckFor === 'any' || $CheckFor === 'all' || $CheckFor === 'NotEqualAny' || $CheckFor === 'NotEqualAll'){
-					foreach ($StmtSearchValue as $value) {
-						$stmt->bindValue(':'.$StmtSearchKey[$i] , $value, PDO::PARAM_STR);
+					foreach ($StmtCondtionValue as $value) {
+						$stmt->bindValue(':'.$StmtCondtionKey[$i] , $value, PDO::PARAM_STR);
 						$i++;
 					}
-				}else if($CheckFor === 'StartLikeAny'){
-					foreach ($StmtSearchValue as $value) {
-						$stmt->bindValue(':'.$StmtSearchKey[$i] , '%'.$value, PDO::PARAM_STR);
+				}else if($CheckFor === 'StartLike'){
+					foreach ($StmtCondtionValue as $value) {
+						$stmt->bindValue(':'.$StmtCondtionKey[$i] , '%'.$value, PDO::PARAM_STR);
 						$i++;
 					}
-				}else if($CheckFor === 'LikeLastAny'){
-					foreach ($StmtSearchValue as $value) {
-						$stmt->bindValue(':'.$StmtSearchKey[$i] , $value.'%', PDO::PARAM_STR);
+				}else if($CheckFor === 'LikeLast'){
+					foreach ($StmtCondtionValue as $value) {
+						$stmt->bindValue(':'.$StmtCondtionKey[$i] , $value.'%', PDO::PARAM_STR);
 						$i++;
 					}
-				}else if($CheckFor === 'StartLikeLastAny'){
-					foreach ($StmtSearchValue as $value) {
-						$stmt->bindValue(':'.$StmtSearchKey[$i] , '%'.$value.'%', PDO::PARAM_STR);
+				}else if($CheckFor === 'StartLikeLast'){
+					foreach ($StmtCondtionValue as $value) {
+						$stmt->bindValue(':'.$StmtCondtionKey[$i] , '%'.$value.'%', PDO::PARAM_STR);
 						$i++;
 					}
 				}else{
-					return ["status"=>"Error","msg"=>"Invalid CheckFor detect","code"=>400];
+					return ["status"=>"Error","msg"=>"Invalid CheckFor details detect!","code"=>400];
 				}
 			}
 			if($CheckUserStatus !== NULL){
@@ -132,16 +175,16 @@
 					}else if($FetchCount === 'all'){
 						return ['status'=>'Success','msg'=>$stmt->fetchAll(),'totalrows'=>$stmt->rowCount(),"code"=>200];
 					}else{
-						return ["status"=>"Error","msg"=>"Invalid FetchCount detect","code"=>400];
+						return ["status"=>"Error","msg"=>"Invalid FetchCount details detect!","code"=>400];
 					}
 				}else{
-					return ['status'=>'Error','msg'=>'Search Data (Given condtion) not found','reason'=>json_encode($stmt->errorinfo()),"code"=>404];
+					return ['status'=>'Error','msg'=>'Given Data not found','reason'=>json_encode($stmt->errorinfo()),"code"=>404];
 				}
 			}else{
-				return ["status"=>"Error","msg"=>"Process failed! Due to some technical error",'reason'=>json_encode($stmt->errorinfo()),"code"=>400];
+				return ["status"=>"Error","msg"=>"Process executetion failed",'reason'=>json_encode($stmt->errorinfo()),"code"=>400];
 			}
 		}else{
-			return ["status"=>"Error","msg"=>"Process failed! beacuse to some invalid data provided",'reason'=>'DatabaseConnection or Table name or search data or required data or EncodeAndEncryptPass found empty',"code"=>400];
+			return ["status"=>"Error","msg"=>"Some technical error occur","code"=>400];
 		}
 	}
 ?>
